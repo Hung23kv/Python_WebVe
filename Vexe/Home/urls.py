5eadb1e95db42a40eb2ee55a005b23aeee018577
Hùng Trịnh <trinhhung23102003@gmail.com>
2025-03-27 11:17:16 +0700

    giao dien nguoi dung

--- Vexe/Home/urls.py
+++ Vexe/Home/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.home,name='home')
+    path('',views.home,name='home'),
+    path('Trip/',views.Trip,name='trip'),
+    path('Detail/',views.Detail,name="detail"),
+    path('Chose_seat/',views.Seat,name = 'seat')
 ]
--- Vexe/Home/views.py
+++ Vexe/Home/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request,'base.html')
+    return render(request,'Home.html')
+def Trip(request):
+    return render(request,'InfoTrip.html')
+def Detail(request):
+    return render(request,'Detail.html')
+def Seat(request):
+    return render(request,'ChoseSeat.html')
